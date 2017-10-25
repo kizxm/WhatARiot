fbf9a1b5f5d3c570585ce672122c1d29972734b3
kira <kizxm@yahoo.com>
2017-10-25 14:42:00 -0700

    added listview

--- app/src/main/java/com/kizxm/whatariot/ChampActivity.java
+++ app/src/main/java/com/kizxm/whatariot/ChampActivity.java
@@ -3,16 +3,41 @@ package com.kizxm.whatariot;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ChampActivity extends AppCompatActivity {
     private TextView mChampionTextView;
+    private ListView mListView;
+    private String[] champions = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_champ);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mChampionTextView = (TextView) findViewById(R.id.championTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, champions);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String champion = ((TextView)view).getText().toString();
+                Toast.makeText(ChampActivity.this, champion, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String champion = intent.getStringExtra("champion");
         mChampionTextView.setText("Champion Data: " + champion);
