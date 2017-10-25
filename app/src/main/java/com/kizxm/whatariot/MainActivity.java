a6eb8a3d9c77b3b74db380e9fc8f82d25c255190
kira <kizxm@yahoo.com>
2017-10-25 13:43:34 -0700

    app logging inputted information

--- app/src/main/java/com/kizxm/whatariot/MainActivity.java
+++ app/src/main/java/com/kizxm/whatariot/MainActivity.java
@@ -1,13 +1,34 @@
 package com.kizxm.whatariot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mChampionButton;
+    private EditText mChampionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mChampionEditText = (EditText) findViewById(R.id.championEditText);
+        mChampionButton = (Button) findViewById(R.id.championButton);
+        mChampionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String champion = mChampionEditText.getText().toString();
+                Log.d(TAG, champion);
+                Intent intent = new Intent(MainActivity.this, ChampActivity.class);
+                        startActivity(intent);
+            }
+        });
     }
 }
