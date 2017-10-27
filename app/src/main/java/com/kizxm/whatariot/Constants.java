6114cd536035bf8c7972d5b054e7fed8d10e81d8
Kira Loo <youhavebeenkiraed@gmail.com>
2017-10-27 16:28:25 -0700

    recording the entered champion to firebase

--- app/src/main/java/com/kizxm/whatariot/Constants.java
+++ app/src/main/java/com/kizxm/whatariot/Constants.java
@@ -18,4 +18,6 @@ public class Constants {
 
     public static final String PREFERENCES_CHAMPION_KEY = "champion";
 
+    public static final String FIREBASE_CHILD_SEARCHED_CHAMPION = "searchedChampion";
+
 }
--- app/src/main/java/com/kizxm/whatariot/ui/MainActivity.java
+++ app/src/main/java/com/kizxm/whatariot/ui/MainActivity.java
@@ -12,16 +12,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.kizxm.whatariot.Constants;
 import com.kizxm.whatariot.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+
+    private DatabaseReference mSearchedChampionReference;
 
     @Bind (R.id.championButton) Button mChampionButton;
     @Bind (R.id.championEditText) EditText mChampionEditText;
@@ -29,6 +34,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mSearchedChampionReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_CHAMPION);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -37,8 +48,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface deliFont = Typeface.createFromAsset(getAssets(), "fonts/delrium.ttf");
         mRiotTextView.setTypeface(deliFont);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
 
         mChampionButton.setOnClickListener(this);
     }
@@ -46,16 +57,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View v) {
                 if (v == mChampionButton) {
+
                     String champion = mChampionEditText.getText().toString();
-                    if(!(champion).equals("")) {
-                        addToSharedPreferences(champion);
-                    }
+
+                    saveChampionToFirebase(champion);
+
+//                    if(!(champion).equals("")) {
+//                        addToSharedPreferences(champion);
+//                    }
+
                     Intent intent = new Intent(MainActivity.this, ChampListActivity.class);
+                    intent.putExtra("champion", champion);
                     startActivity(intent);
                 }
             }
 
-        private void addToSharedPreferences(String champion) {
-        mEditor.putString(Constants.PREFERENCES_CHAMPION_KEY, champion).apply();
+//        private void addToSharedPreferences(String champion) {
+//        mEditor.putString(Constants.PREFERENCES_CHAMPION_KEY, champion).apply();
+//    }
+
+    public void saveChampionToFirebase(String champion) {
+        mSearchedChampionReference.push().setValue(champion);
     }
 }
